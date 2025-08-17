c799f7178ce8ec663fdf0cea5cce466c1289ce98
JakubJarosz <95305824+JakubJarosz@users.noreply.github.com>
2025-08-17 11:36:31 +0200

    add

--- src/services/apiSlices.js
+++ src/services/apiSlices.js
@@ -4,23 +4,24 @@ export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://reqres.in/api/products',
+    prepareHeaders: (headers) => {
+      headers.set('x-api-key', 'reqres-free-v1'); 
+      return headers;
+    },
   }),
   tagTypes: ['Items'],
-  endpoints: builder => ({
+  endpoints: (builder) => ({
     getItems: builder.query({
-      query: args => {
-        const { page, id } = args;
-        return {
-          url: `?per_page=5&page=${page}&id=${id}`,
-        };
-      },
+      query: ({ page, id }) => ({
+        url: `?per_page=5&page=${page}&id=${id}`,
+      }),
       providesTags: ['Items'],
     }),
     getAllItems: builder.query({
-      query: () => `?per_page=12`,
+      query: () => '?per_page=12',
       providesTags: ['Items'],
     }),
   }),
 });
 
-export const { useGetItemsQuery, useGetAllItemsQuery} = apiSlice;
+export const { useGetItemsQuery, useGetAllItemsQuery } = apiSlice;
